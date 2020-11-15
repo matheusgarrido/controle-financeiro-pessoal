72cbe92d9fca8e6127254bd6c39ab3188d963130
Matheus <matheusgarrido10@hotmail.com>
2020-11-15 11:55:14 -0300

    url api fixed

--- client/src/services/transactionService.js
+++ client/src/services/transactionService.js
@@ -1,34 +1,34 @@
 import axios from 'axios';
-const webURL =
-  'https://controle-financeiro-fullstack.herokuapp.com/api/transaction';
+const webURL = !process.env.REACT_APP_API_URL
+  ? process.env.REACT_APP_API_URL
+  : 'https://controle-financeiro-fullstack.herokuapp.com/api/transaction';
 
 async function getAllMonthYear() {
-  const URL = process.env.REACT_APP_API_URL || webURL + '/allYearMonth';
+  const URL = webURL + '/allYearMonth';
   const res = await axios.get(URL);
   return res.data;
 }
 
 async function getOneMonthYear(period) {
-  const URL = process.env.REACT_APP_API_URL || webURL + '?period=' + period;
+  const URL = webURL + '?period=' + period;
   const res = await axios.get(URL);
   return res.data;
 }
 
 async function deleteTransaction(id) {
-  const URL = process.env.REACT_APP_API_URL || webURL + '/' + id;
+  const URL = webURL + '/' + id;
   const res = await axios.delete(URL);
   return res.data;
 }
 
 async function patchTransaction(id, data) {
-  const URL = process.env.REACT_APP_API_URL || webURL + '/' + id;
+  const URL = webURL + '/' + id;
   const res = await axios.patch(URL, data);
   return res.data;
 }
 
 async function createTransaction(data) {
-  const URL = process.env.REACT_APP_API_URL || webURL;
-  console.log(data);
+  const URL = webURL;
   const res = await axios.post(URL, data);
   return res.data;
 }
